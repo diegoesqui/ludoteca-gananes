15dd48c99cd6b6c015630ac01aeacdc11686dd2c
Diego Esquinazi <diegoesqui@MacBookPro-001.lan>
2025-07-08 19:38:26 +0200

    feat: Implementar barra de filtros dinámica

--- js/ui.js
+++ js/ui.js
@@ -5,3 +5,18 @@ const closeDetailsModalBtn = document.getElementById('close-details-modal-btn');
 
 closeGameModalBtn.addEventListener('click', () => gameModal.classList.add('hidden'));
 closeDetailsModalBtn.addEventListener('click', () => gameDetailsModal.classList.add('hidden'));
+
+// --- SCROLL BEHAVIOR FOR FILTER BAR ---
+let lastScrollY = window.scrollY;
+const filterBar = document.getElementById('filter-bar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > lastScrollY && window.scrollY > 200) {
+        // Scrolling down & past a certain point
+        filterBar.classList.add('hidden-bar');
+    } else {
+        // Scrolling up
+        filterBar.classList.remove('hidden-bar');
+    }
+    lastScrollY = window.scrollY;
+});
